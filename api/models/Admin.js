8a3b948f888447f4c268cb9d4b91972ac059795b
karmasakshi <karmasakshi@outlook.com>
2015-02-27 18:09:20 +0530

    Typo.

--- api/models/Admin.js
+++ api/models/Admin.js
@@ -7,30 +7,28 @@
 
 module.exports = {
     attributes: {
-        attributes: {
-            // Username of the admin
-            username: {
-                type: 'string',
-                required: true,
-                size: 20
-            },
-            // Hashed password of the admin
-            password: {
-                type: 'string',
-                required: true
-            },
-            // Sessions associated with the admin
-            sessions: {
-                collection: 'session',
-                via: 'admin'
-            },
-            // Active flag
-            active: {
-                type: 'boolean',
-                defaultsTo: 1
-            }
+        // Username of the admin
+        username: {
+            type: 'string',
+            required: true,
+            size: 20
         },
-        schema: true
-    }
+        // Hashed password of the admin
+        password: {
+            type: 'string',
+            required: true
+        },
+        // Sessions associated with the admin
+        sessions: {
+            collection: 'session',
+            via: 'admin'
+        },
+        // Active flag
+        active: {
+            type: 'boolean',
+            defaultsTo: 1
+        }
+    },
+    schema: true
 };
 
